1c57bd428fe8102af48bb675ab232e27df1c8442
Camille Blondiaux <camille.blondiaux@gmail.com>
2024-12-01 19:57:48 +0100

    Allègement du site
    Changement de la police
    Ajout d'un header
    Suppression du JS et des animations

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -3,39 +3,35 @@ $(function () {
     $('.menu').on('click', function () {
         $(this).toggleClass('active');
         if ($(this).hasClass('active')) {
-            $('.ss-menu-home').addClass('visible1');
-            $('.ss-menu-music').addClass('visible2');
-            $('.ss-menu-band').addClass('visible3');
-            $('.ss-menu-shows').addClass('visible4');
-            $('.ss-menu-contact').addClass('visible5');
+            $('.ss-menu-music').addClass('visible1');
+            $('.ss-menu-band').addClass('visible2');
+            $('.ss-menu-shows').addClass('visible3');
+            $('.ss-menu-contact').addClass('visible4');
         } else {
-            $('.ss-menu-home').removeClass('visible1');
-            $('.ss-menu-music').removeClass('visible2');
-            $('.ss-menu-band').removeClass('visible3');
-            $('.ss-menu-shows').removeClass('visible4');
-            $('.ss-menu-contact').removeClass('visible5');
+            $('.ss-menu-music').removeClass('visible1');
+            $('.ss-menu-band').removeClass('visible2');
+            $('.ss-menu-shows').removeClass('visible3');
+            $('.ss-menu-contact').removeClass('visible4');
         }
     })
 })
 $(function () {
     $('.ss-menu').on('click', function () {
       $('.menu').removeClass('active');
-      $('.ss-menu-home').removeClass('visible1');
-      $('.ss-menu-music').removeClass('visible2');
-      $('.ss-menu-band').removeClass('visible3');
-      $('.ss-menu-shows').removeClass('visible4');
-      $('.ss-menu-contact').removeClass('visible5');
+      $('.ss-menu-music').removeClass('visible1');
+      $('.ss-menu-band').removeClass('visible2');
+      $('.ss-menu-shows').removeClass('visible3');
+      $('.ss-menu-contact').removeClass('visible4');
     })
 })
 $(function () {
     $(window).on('scroll', function () {
         if ($('.menu').hasClass('active')) {
           $('.menu').removeClass('active');
-          $('.ss-menu-home').removeClass('visible1');
-          $('.ss-menu-music').removeClass('visible2');
-          $('.ss-menu-band').removeClass('visible3');
-          $('.ss-menu-shows').removeClass('visible4');
-          $('.ss-menu-contact').removeClass('visible5');
+          $('.ss-menu-music').removeClass('visible1');
+          $('.ss-menu-band').removeClass('visible2');
+          $('.ss-menu-shows').removeClass('visible3');
+          $('.ss-menu-contact').removeClass('visible4');
         }
     })
 })
@@ -54,14 +50,6 @@ window.addEventListener("load", function () {
   }
 })
 
-// Manage vidéo
-$(function () {
-    $('video').on('click', function(event) {
-      event.preventDefault();
-      document.getElementById('tucoVideo').play();
-    });
-})
-
 // resize reload
 $(function () {
   let initialWidth = $(window).innerWidth();
@@ -71,66 +59,4 @@ $(function () {
       document.location.reload(true);
     }
   })
-})
-
-// Manage scroll up button
-$(function () {
-    let ecran = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-      $(window).on('scroll', function () {
-        let scrollNow = $(window).scrollTop();
-        $(window).on('scroll', function functionName() {
-          if (scrollNow > 600 && scrollNow > $(window).scrollTop()) {
-            if ($('#upArrow').is(":hidden")) {
-              $('#upArrow').show();
-            }
-          } else {
-            $('#upArrow').hide();
-          }
-        })
-        $('#upArrow').on('click', function () {
-            $(window).scrollTop(0);
-        })
-      })
-})
-
-// Delete scroll tag on scroll down
-$(function () {
-    $(window).on('scroll', function () {
-        let topPage = $(window).scrollTop();
-        if (topPage >= 150) {
-          $('#scrollDown').hide();
-        } else {
-          $('#scrollDown').show();
-        }
-    })
-})
-// Manage tag scroll down
-$(function () {
-    $('#scrollDown').on('click', function() {
-      window.location.href = "#nextShow";
-    });
-})
-
-// Locations
-$(function () {
-    $(".card").on('click', () => {});
-})
-// Location socials
-$(function () {
-  $('.facebook').on('click', function(event) {
-    event.preventDefault();
-    window.location.href = "https://www.facebook.com/NousSommesLaDebacle/";
-  });
-  $('.instagram').on('click', function(event) {
-    event.preventDefault();
-    window.location.href = "https://instagram.com/ladebacleinsta";
-  });
-  $('.soundcloud').on('click', function(event) {
-    event.preventDefault();
-    window.location.href = "https://soundcloud.com/ladebacle";
-  });
-  $('.bandcamp').on('click', function(event) {
-    event.preventDefault();
-    window.location.href = "https://ladebacle.bandcamp.com/";
-  });
 })
